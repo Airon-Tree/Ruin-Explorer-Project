f45426bd57b4b0013e1616387910b492d80147e2
unknown <hf954@nyu.edu>
2025-11-20 00:22:25 -0500

    BlueScreen

---
+++ blue_screen.gd
@@ -0,0 +1,55 @@
+extends Node2D
+
+@onready var area = $Area2D
+@onready var blue_screen_panel = $CanvasLayer/BlueScreenPanel
+@onready var label = $CanvasLayer/BlueScreenPanel/Label
+
+@export_multiline var screen_text: String = "TEMPLATE, press E to exit"
+@export var interaction_hint: String = "[E] 查看"
+
+var player_nearby = false
+var screen_open = false
+
+func _ready():
+	# 初始隐藏蓝屏
+	blue_screen_panel.hide()
+	
+	# 连接信号
+	area.body_entered.connect(_on_body_entered)
+	area.body_exited.connect(_on_body_exited)
+	
+	# 设置文字
+	label.text = screen_text
+
+func _process(_delta):
+	# 检测E键
+	if Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("interact"):
+		if player_nearby:
+			toggle_screen()
+
+func _on_body_entered(body):
+	# 检查节点名字
+	if body.name == "Player":  # 改成你们玩家的实际名字
+		player_nearby = true
+		print("玩家靠近 BlueScreen")
+
+func _on_body_exited(body):
+	if body.name == "Player":
+		player_nearby = false
+		print("玩家离开 BlueScreen")
+		if screen_open:
+			close_screen()
+
+func toggle_screen():
+	if screen_open:
+		close_screen()
+	else:
+		open_screen()
+
+func open_screen():
+	screen_open = true
+	blue_screen_panel.show()
+
+func close_screen():
+	screen_open = false
+	blue_screen_panel.hide()
