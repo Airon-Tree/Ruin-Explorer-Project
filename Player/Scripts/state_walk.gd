e27434216e9fd2eba00725f8c1527fab1ae8991e
Airon-Tree <jd5943@nyu.edu>
2025-12-01 00:20:06 -0500

    running

--- Player/Scripts/state_walk.gd
+++ Player/Scripts/state_walk.gd
@@ -1,7 +1,9 @@
 class_name State_Walk
 extends State
 
-@export var move_speed : float = 110.0
+@export var move_speed : float = 100.0
+@export var run_speed_multiplier : float = 1.5
+@export var run_anim_speed_multiplier : float = 1.5
 
 @onready var idle : State = $"../Idle"
 @onready var attack : State = $"../Attack"
@@ -9,16 +11,33 @@ extends State
 
 func enter() -> void:
 	player.update_animation("walk")
+	if player.sprite:
+		player.sprite.speed_scale = 1.0
 	pass
 	
 func exit() -> void:
+	if player.sprite:
+		player.sprite.speed_scale = 1.0
 	pass
 	
 func process( _delta : float ) -> State:
 	if player.direction == Vector2.ZERO:
+		if player.sprite:
+			player.sprite.speed_scale = 1.0
 		return idle
 	
-	player.velocity = player.direction * move_speed
+	
+	var speed := move_speed
+	var anim_speed := 1.0
+	
+	if Input.is_action_pressed("run"):
+		speed *= run_speed_multiplier
+		anim_speed = run_anim_speed_multiplier
+	
+	player.velocity = player.direction * speed
+	
+	if player.sprite:
+		player.sprite.speed_scale = anim_speed
 	
 	if player.set_direction():
 		player.update_animation("walk")
