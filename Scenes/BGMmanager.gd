9f6d34be7d3239ebb24d86e929e4726c9299e379
Airon-Tree <jd5943@nyu.edu>
2025-12-15 17:28:39 -0500

    bgm loop

--- Scenes/BGMmanager.gd
+++ Scenes/BGMmanager.gd
@@ -3,12 +3,26 @@ extends Node
 
 @onready var player = $AudioStreamPlayer
 
-func play_bgm(music: AudioStream):
+func _ready() -> void:
+	if player.stream:
+		player.stream.loop = true
+	player.finished.connect(_on_player_finished)
+
+
+func play_bgm(music: AudioStream) -> void:
 	if player.stream != music:
 		player.stream = music
+		player.stream.loop = true
+	
+	if not player.playing:
 		player.play()
 
-func stop_bgm():
+
+func _on_player_finished() -> void:
+	if player.stream:
+		player.play()
+
+
+func stop_bgm() -> void:
 	player.stop()
-	
 	
